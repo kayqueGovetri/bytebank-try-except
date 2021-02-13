b77551c0d3fee873ddee2e5846492e59fa7823b0
kayqueGovetri <govetrikayque@gmail.com>
2021-02-13 11:09:51 -0300

    Example except types

--- example.py
+++ example.py
@@ -1,20 +1,22 @@
 def dividir(dividendo, divisor):
-    print(divisor.resultado)
+    # print(divisor.resultado)
     return dividendo / divisor
 
 
 def testa_divisao(divisor):
-    try:
-        resultado = dividir(10, divisor)
-        print(f"O resultado da divisão de 10 por {divisor} é {resultado}")
-    except ZeroDivisionError:
-        print("Erro de divisão pro 0 tratado.")
-    except TypeError:
-        print("Erro de divisão por string tratado.")
+    resultado = dividir(10, divisor)
+    print(f"O resultado da divisão de 10 por {divisor} é {resultado}")
 
 
 try:
-    testa_divisao(50)
-except AttributeError:
+    testa_divisao(0)
+except AttributeError as error:
+    print(error.__class__.__bases__)
     print("Erro de atributo tratado.")
+except ZeroDivisionError as error:
+    print(error.__class__.__bases__)
+    print("Erro de divisão pro 0 tratado.")
+except TypeError as error:
+    print(error.__class__.__bases__)
+    print("Erro de divisão por string tratado.")
 print("Programa encerrado.")
